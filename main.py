85d8cf3dcb4706f6040a8dd2890cc0dfae0a0062
Andrey Borisov <113293792+kala7adx@users.noreply.github.com>
2024-02-19 19:46:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import base64
-import os
 
+# Writing the file
 #bytes = bytearray("print(dict(__import__('os').environ))", 'utf-8')
 
 #base64string = base64.b64encode(bytes)
@@ -9,6 +9,7 @@ import os
 
 #f.write(base64string.decode('utf-8'))
 
+# Reading the file
 f = open('harmless.py', 'r')
 
 eval(base64.b64decode(f.readline()).decode('utf-8'))
